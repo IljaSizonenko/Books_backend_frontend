22e55ab1ebecca321c41feee180dfd2f74bc1a40
IljaSizonenko <spongeilja@gmail.com>
2026-04-01 23:37:48 +0300

    Ignore Book API Mock before subtree import

--- Book API Mock/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import bookRoutes from "./book.routes.js";
-import reviewRoutes from "./review.routes.js";
-
-const router = Router();
-router.use("/books", reviewRoutes);
-router.use("/books", bookRoutes);
-export default router
--- Book API Mock/src/utils/filter.utils.ts
+++
@@ -1,7 +0,0 @@
-export function contains(value: string, search?: string) {
-    if (!value) return false
-    return !search || value.toLowerCase().includes(search.toLowerCase());
-}
-export function equals<T>(value: T, search?: T) {
-    return search === undefined || value === search;
-}
